f6097564626b080ff9d33e427faa5766ca9182b5
AdamKing11 <adamking@email.arizona.edu>
2016-12-01 00:39:55 -0700

    now with Convolutional layers

--- w.py
+++ w.py
@@ -23,22 +23,30 @@ def load_CEG(ceg_file):
 				mf.append((line[0], line[1]))
 	return mf 
 			
-def vectorize(wl):
+def vectorize(wl, by_char = True):
 
 	char_to_int = {}
 	int_to_char = {}
 	vl = []
 
 	for w in wl:
-		v = []
-		for c in w:
-			if c not in char_to_int:
-				c_ind = len(char_to_int) + 1
-				char_to_int[c] = c_ind
-				int_to_char[c_ind] = c
-			v.append(char_to_int[c])
-		vl.append(v)
-		v = []
+		if by_char:
+			v = []
+			for c in w:
+				if c not in char_to_int:
+					c_ind = len(char_to_int) + 1
+					char_to_int[c] = c_ind
+					int_to_char[c_ind] = c
+				v.append(char_to_int[c])
+			vl.append(v)
+			v = []
+		else:
+			if w not in char_to_int:
+				w_ind = len(char_to_int) + 1
+				char_to_int[w] = [w_ind]
+				int_to_char[w_ind] = w
+			vl.append(char_to_int[w])
+	
 	return vl, int_to_char
 
 def longest_word(l):
@@ -51,14 +59,14 @@ def format_data(data, split = .75, shuf = False):
 		shuffle(data)
 
 	X = [x[0] for x in data]
-	Y = [y[1][1] for y in data]	# so we just predict the second character, m/f
+	Y = [y[1] for y in data]	# so we just predict the second character, m/f
 
 	x_vec, x_itc = vectorize(X)
-	y_vec, y_itc = vectorize(Y)
+	y_vec, y_itc = vectorize(Y, by_char = False)
 
 	max_x_len = longest_word(x_vec)
 	max_y_len = longest_word(y_vec)
-
+	
 	x_vec = sequence.pad_sequences(x_vec, maxlen=max_x_len)
 	y_vec = sequence.pad_sequences(y_vec, maxlen=max_y_len)
 	
@@ -75,74 +83,46 @@ def format_data(data, split = .75, shuf = False):
 	return (x_train, y_train), (x_test, y_test), (max_x_len, max_y_len)
 
 
+def build_model_1(num_words):
+	embed_len = 64
+	convo_filters = 32
+	filt_len = 3
+	pool_len = 3
+	lstm_size = 16
+
+	model = Sequential()
+	# embedding
+	model.add(Embedding(num_words, embed_len, input_length=x_longest))
+	# convolution/maxpool
+	model.add(Convolution1D(nb_filter=convo_filters, filter_length=filt_len, border_mode='same', activation='relu'))
+	model.add(MaxPooling1D(pool_length=pool_len))
+	# dropout and LSTM
+	model.add(Dropout(0.25))
+	model.add(Bidirectional(LSTM(lstm_size))) # unroll to make faster?
+	model.add(Dropout(0.25))
+	# final layer
+	model.add(Dense(y_longest, activation='sigmoid'))
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+	return model
+
 
 # gotta beat 74%
 
 
 data = load_CEG("CEG.NOUNS.txt")
+#data = data[:15000]
+
 (x_tr, y_tr), (x_te, y_te), (x_longest, y_longest) = format_data(data, shuf = True)
+	
+epochs = 5
 
-embed_len = 32
-num_words = x_tr.shape[0]
-#print(x_tr.shape, y_tr.shape)
-#print(x_te.shape, y_te.shape)
-#print(x_tr[0], y_tr[0])
-
-
-print("Starting model...")
-model = Sequential()
-
-print("Embedding...")
-model.add(Embedding(num_words, embed_len, input_length=x_longest))
-
-print("Convolution and...", end = "")
-model.add(Convolution1D(nb_filter=16, filter_length=2, border_mode='same', activation='relu'))
-print("Pooling.")
-model.add(MaxPooling1D(pool_length=2))
-
-print("A pinch of dropout...")
-model.add(Dropout(0.4))
-
-print("LSTM (goes both ways ;) )")
-model.add(Bidirectional(LSTM(64))) # unroll to make faster?
-#model.add(Dense(8))
-model.add(Dropout(0.4))
-print("Final layer, Dense")
-model.add(Dense(y_longest, activation='sigmoid'))
-
-print("Now, to compile!")
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+print("Building model...")
+model = build_model_1(num_words = x_tr.shape[0])
+print("Done. Now to train!")
 #print(model.summary())
 
-model.fit(x_tr, y_tr, validation_data=(x_te, y_te), nb_epoch=1, batch_size=64)
+model.fit(x_tr, y_tr, nb_epoch=epochs, batch_size=256)
 
 scores = model.evaluate(x_te, y_te, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-masc_vec, mvec_dict = vectorize(masc)
-fem_vec, fvec_dict = vectorize(fem)
-longest_word = longest_word(masc_vec+fem_vec)
-
- = masc_vec + fem_vec
-
-
-for c in masc_vec[0]:
-	print(c, mvec_dict[c], end="\t")
-print()
-print(len(masc), len(fem))
-print(longest_word)
-
-"""
